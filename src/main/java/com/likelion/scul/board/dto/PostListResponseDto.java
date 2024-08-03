6880c82f94124b9aa7b35968df5d7f8e30cea47c
fnsl1026 <fnsl1026@naver.com>
2024-08-03 16:26:56 +0900

    refactor: add @Data to PostListResponseDto

--- src/main/java/com/likelion/scul/board/dto/PostListResponseDto.java
+++ src/main/java/com/likelion/scul/board/dto/PostListResponseDto.java
@@ -1,7 +1,10 @@
 package com.likelion.scul.board.dto;
 
+import lombok.Data;
+
 import java.util.List;
 
+@Data
 public class PostListResponseDto {
     private List<PostListDto> posts; // 페이징된 게시물 목록
     private int totalPosts; // 총 게시물 수
