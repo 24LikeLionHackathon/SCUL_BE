96ad64a92d825090a7e8bde2846cf8685ccce76a
fnsl1026 <fnsl1026@naver.com>
2024-08-03 16:58:59 +0900

    refactor: add constructor to PostListResponseDto

--- src/main/java/com/likelion/scul/board/dto/PostListResponseDto.java
+++ src/main/java/com/likelion/scul/board/dto/PostListResponseDto.java
@@ -9,6 +9,11 @@ public class PostListResponseDto {
     private List<PostListDto> posts; // 페이징된 게시물 목록
     private int totalPosts; // 총 게시물 수
 
-    public PostListResponseDto(List<PostListDto> paginatedPosts, int totalPosts) {
+    public PostListResponseDto(List<PostListDto> posts, int totalPosts) {
+        this.posts = posts;
+        this.totalPosts = totalPosts;
+    }
+
+    public PostListResponseDto() {
     }
 }
