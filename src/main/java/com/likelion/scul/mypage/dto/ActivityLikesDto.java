3462deaeffc5c03e7f2a82d3bbe2af1259265fb7
fnsl1026 <fnsl1026@naver.com>
2024-08-06 02:30:29 +0900

    refactor : add @Data                @AllArgsConstructor                @NoArgsConstructor to make it clear in myPage DTO

--- src/main/java/com/likelion/scul/mypage/dto/ActivityLikesDto.java
+++ src/main/java/com/likelion/scul/mypage/dto/ActivityLikesDto.java
@@ -16,14 +16,6 @@ public class ActivityLikesDto {
     private int totalLikes;
     private int participatingClubs;
 
-    public ActivityLikesDto(List<LikedPostInfoDto> postsList, int totalPosts, int totalComments, int totalLikes, int participatingClubs) {
-        this.postsList = postsList;
-        this.totalPosts = totalPosts;
-        this.totalComments = totalComments;
-        this.totalLikes = totalLikes;
-        this.participatingClubs = participatingClubs;
-    }
-
 }
 
 
--- src/main/java/com/likelion/scul/mypage/dto/LikedPostInfoDto.java
+++ src/main/java/com/likelion/scul/mypage/dto/LikedPostInfoDto.java
@@ -17,14 +17,5 @@ public class LikedPostInfoDto {
     private LocalDateTime createdAt;
     private int commentCount;
 
-    public LikedPostInfoDto(String postImage, String sportsName, String boardName, String postTitle, String postAuthorNickname, LocalDateTime createdAt, int commentCount) {
-        this.postImage = postImage;
-        this.sportsName = sportsName;
-        this.boardName = boardName;
-        this.postTitle = postTitle;
-        this.postAuthorNickname = postAuthorNickname;
-        this.createdAt = createdAt;
-        this.commentCount = commentCount;
-    }
 }
 
