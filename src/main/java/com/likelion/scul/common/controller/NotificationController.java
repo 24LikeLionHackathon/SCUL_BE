6f04697a0b05e83a31df890a126d6bc702dda916
Han <hkjbrian@naver.com>
2024-08-05 19:42:11 +0900

    Notification : UserId를 통한 RequestParam accessToken 활용하도록 수정

--- src/main/java/com/likelion/scul/common/controller/NotificationController.java
+++ src/main/java/com/likelion/scul/common/controller/NotificationController.java
@@ -1,49 +1,62 @@
 package com.likelion.scul.common.controller;
 
+import com.likelion.scul.auth.service.UserService;
 import com.likelion.scul.common.domain.Notification;
 import com.likelion.scul.common.dto.notification.NotificationRequest;
 import com.likelion.scul.common.dto.notification.NotificationResponse;
 import com.likelion.scul.common.service.NotificationService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/notification")
 public class NotificationController {
     private NotificationService notificationService;
+    private UserService userService;
 
-    public NotificationController(NotificationService notificationService) {
+    public NotificationController(
+            NotificationService notificationService,
+            UserService userService
+    ) {
         this.notificationService = notificationService;
+        this.userService = userService;
     }
 
-    @GetMapping("/all/{userId}/{pageNum}")
-    public ResponseEntity<NotificationResponse> getNotifications(@PathVariable(value = "userId") Long userId, @PathVariable(value = "pageNum") int pageNum) {
+    @GetMapping("/all/{pageNum}")
+    public ResponseEntity<NotificationResponse> getNotifications(@PathVariable(value = "pageNum") int pageNum, HttpServletRequest request) {
+        Long userId = userService.extractUserIdByAccessToken(request);
+
         NotificationResponse response = new NotificationResponse();
         response.setNotifications(notificationService.getNotifications(userId, pageNum));
         response.setNotificationCount(notificationService.getCount(userId));
+
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/read/{userId}/{pageNum}")
-    public ResponseEntity<NotificationResponse> getReadNotifications(@PathVariable(value = "userId") Long userId, @PathVariable(value = "pageNum") int pageNum) {
+    @GetMapping("/read/{pageNum}")
+    public ResponseEntity<NotificationResponse> getReadNotifications(@PathVariable(value = "pageNum") int pageNum, HttpServletRequest request) {
+        Long userId = userService.extractUserIdByAccessToken(request);
+
         NotificationResponse response = new NotificationResponse();
         response.setNotifications(notificationService.getReadNotifications(userId, pageNum));
         response.setNotificationCount(notificationService.getReadCount(userId));
+
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/unread/{userId}/{pageNum}")
-    public ResponseEntity<NotificationResponse> getUnReadNotifications(@PathVariable(value = "userId") Long userId, @PathVariable(value = "pageNum") int pageNum) {
+    @GetMapping("/unread/{pageNum}")
+    public ResponseEntity<NotificationResponse> getUnReadNotifications(@PathVariable(value = "pageNum") int pageNum, HttpServletRequest request) {
+        Long userId = userService.extractUserIdByAccessToken(request);
+
         NotificationResponse response = new NotificationResponse();
         response.setNotifications(notificationService.getUnReadNotifications(userId, pageNum));
         response.setNotificationCount(notificationService.getUnReadCount(userId));
+
         return ResponseEntity.ok(response);
     }
 
-    // 테스트 통과
     @PostMapping("/test")
     public ResponseEntity<Notification> saveNotification(@RequestBody NotificationRequest request) {
         return ResponseEntity.ok(notificationService.saveNotification(request));
