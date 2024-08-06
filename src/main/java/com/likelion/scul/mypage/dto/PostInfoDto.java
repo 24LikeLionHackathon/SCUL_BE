fcffe350c3843ad54fb53cb5d83b6feefdeb775a
fnsl1026 <fnsl1026@naver.com>
2024-08-06 19:54:52 +0900

    refactor : add @Data                @AllArgsConstructor                @NoArgsConstructor to postId

--- src/main/java/com/likelion/scul/mypage/dto/PostInfoDto.java
+++ src/main/java/com/likelion/scul/mypage/dto/PostInfoDto.java
@@ -1,7 +1,15 @@
 package com.likelion.scul.mypage.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PostInfoDto {
     private String postImage;
     private String sportsName;
@@ -11,64 +19,4 @@ public class PostInfoDto {
     private int commentCount;
 
     private long postId;
-
-    public PostInfoDto(String postImage, String sportsName, String boardName, String postTitle, LocalDateTime createdAt, int commentCount, long postId) {
-        this.postImage = postImage;
-        this.sportsName = sportsName;
-        this.boardName = boardName;
-        this.postTitle = postTitle;
-        this.createdAt = createdAt;
-        this.commentCount = commentCount;
-        this.postId = postId;
-    }
-
-// Getters and Setters
-
-    public String getPostImage() {
-        return postImage;
-    }
-
-    public void setPostImage(String postImage) {
-        this.postImage = postImage;
-    }
-
-    public String getSportsName() {
-        return sportsName;
-    }
-
-    public void setSportsName(String sportsName) {
-        this.sportsName = sportsName;
-    }
-
-    public String getBoardName() {
-        return boardName;
-    }
-
-    public void setBoardName(String boardName) {
-        this.boardName = boardName;
-    }
-
-    public String getPostTitle() {
-        return postTitle;
-    }
-
-    public void setPostTitle(String postTitle) {
-        this.postTitle = postTitle;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public int getCommentCount() {
-        return commentCount;
-    }
-
-    public void setCommentCount(int commentCount) {
-        this.commentCount = commentCount;
-    }
 }
